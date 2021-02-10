0380c918f68a13d8d1576f8860838839668e7a4b
Jerome Laban <jerome.laban@nventive.com>
2021-02-10 07:31:50 -0500

    Add project files.

---
+++ UnoSkiaSharpFiddle/UnoSkiaSharpFiddle.Shared/Compiler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Host;
+
+namespace UnoSkiaSharpFiddle
+{
+    public class Compiler
+    {
+        public static Assembly Compile(string source)
+        {
+            var cus = SyntaxFactory.ParseCompilationUnit(source);
+
+            var sourceLanguage = CSharpLanguage.Instance;
+
+            Compilation compilation = sourceLanguage
+              .CreateLibraryCompilation(assemblyName: "InMemoryAssembly", enableOptimisations: false)
+              .AddSyntaxTrees(new[] { cus.SyntaxTree });
+
+            Console.WriteLine($"Got compilation");
+
+            // GetDiagnostics seems to keep running in a CPU Bound loop
+            Console.WriteLine($"Got compilation Diagnostics: {compilation.GetDiagnostics().Length}");
+            Console.WriteLine($"Got compilation DeclarationDiagnostics: {compilation.GetDeclarationDiagnostics().Length}");
+
+            Console.WriteLine($"Emitting assembly...");
+            var stream = new MemoryStream();
+            var emitResult = compilation.Emit(stream);
+
+            if (emitResult.Success)
+            {
+                Console.WriteLine($"Got binary assembly: {emitResult.Success}");
+
+                return Assembly.Load(stream.ToArray());
+            }
+            else
+            {
+                foreach (var diagnostic in emitResult.Diagnostics)
+                {
+                    Console.WriteLine(diagnostic);
+                }
+
+                throw new InvalidOperationException("Failed to emit assembly");
+            }
+        }
+    }
+
+    public class CSharpLanguage : ILanguageService
+    {
+        private readonly IEnumerable<MetadataReference> _references;
+
+        public static CSharpLanguage Instance { get; } = new CSharpLanguage();
+
+        private CSharpLanguage()
+        {
+            var sdkFiles = this.GetType().Assembly.GetManifestResourceNames().Where(f => f.Contains("dotnet-sdk"));
+
+            _references = sdkFiles
+                .Select(f =>
+                {
+                    using (var s = this.GetType().Assembly.GetManifestResourceStream(f))
+                    {
+                        return MetadataReference.CreateFromStream(s);
+                    }
+                })
+                .ToArray();
+        }
+
+        public Compilation CreateLibraryCompilation(string assemblyName, bool enableOptimisations)
+        {
+            var options = new CSharpCompilationOptions(
+                OutputKind.DynamicallyLinkedLibrary,
+                optimizationLevel: OptimizationLevel.Release,
+                allowUnsafe: true)
+                // Disabling concurrent builds allows for the emit to finish.
+                .WithConcurrentBuild(false)
+                ;
+
+            return CSharpCompilation.Create(assemblyName, options: options, references: _references);
+        }
+    }
+
+}
---
+++ UnoSkiaSharpFiddle/UnoSkiaSharpFiddle.Shared/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.UWP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoSkiaSharpFiddle
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Action<SKSurface> _paint;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            source.Text = @"
+using SkiaSharp;
+using SkiaSharp.Views.UWP;
+
+public class Program 
+{ 
+    public static void Paint(SKSurface surface)
+    {
+        surface.Canvas.Clear(SKColors.Blue);
+    }
+}
+";
+        }
+
+        public void OnCompile()
+        {
+            var assembly = Compiler.Compile(source.Text);
+
+            if (assembly.GetExportedTypes().Where(et => et.Name == "Program").FirstOrDefault() is Type programType)
+            {
+                Console.WriteLine("Got Program type");
+
+                if (programType.GetMethod("Paint") is MethodInfo paintMethod)
+                {
+                    Console.WriteLine("Got Main method");
+
+                    _paint = (Action<SKSurface>)paintMethod.CreateDelegate(typeof(Action<SKSurface>));
+                }
+            }
+        }
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs arg)
+        {
+            _paint(arg.Surface);
+        }
+    }
+}
